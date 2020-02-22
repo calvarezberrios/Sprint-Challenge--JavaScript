dc8294ecb766ea5f50a76025d6abf5e9d596df71
Mannie <carlos.alvarezberrios@gmail.com>
2020-02-22 12:38:00 -0500

    Arrays Request 3 completed successfully

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -97,6 +97,11 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+for(let key in graduates) {
+  if(graduates[key].university.match("Uni")) {
+    unisWithUni.push(graduates[key]);
+  }
+}
 console.log(unisWithUni);
 
 
